e44cec53c7a19e707e51f433bfc6d3a878a89806
Niladri Chatterjee <niladrichatterjee365@gmail.com>
2023-10-14 21:08:24 +0530

    Update

---
+++ Practice C++/Debugging/Chef On Date.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int t,x,z;
+cin>>t;
+while (t--)
+{
+    cin>>x>>z;
+    if (x>=z)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+}
+	return 0;
+}
---
+++ Practice C++/Debugging/Chef and Brain Speed.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X,Y;
+	cin>>X>>Y;
+	if(X<Y){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+	return 0;
+}
